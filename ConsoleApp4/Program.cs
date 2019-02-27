a2a3a954231b4ab70b7af068fbd41f679439b09a
visualstudio <visualstudio@122-PC24>
2019-02-27 09:51:00 -0500

    commit 2

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -15,4 +15,32 @@ namespace ConsoleApp4
             orderProcessor.Process(order);
         }
     }
+
+    public class OrderProcessor
+    {
+        private readonly ShippingCalculator _shippingCalculator;
+        public OrderProcessor()
+        {
+            _shippingCalculator = new ShippingCalculator();
+        }
+        public void Process(Order order)
+        {
+            if (order.IsShipped)
+                throw new InvalidOperationException("This order is already processed.");
+            order.Shipment = new Shipment
+            {
+                Cost = _shippingCalculator.CalculateShipping(order),
+                ShippingDate = DateTime.Today.AddDays(1);
+        }
+    }
+
+    public class ShippingCalculator
+    {
+        public float CalculateShipping(Order order)
+        {
+
+            if (order.TotalPrice < 30f) return order.TotalPrice * 0.1f;
+            return 0;
+        }
+    }
 }
